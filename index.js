bbda153d44b158bd0d9f9a1573cb8459733f3264
Nevin Katz <nevkatz@gmail.com>
2021-02-27 11:53:15 -0500

    big progress with the file system commands
    - commented out the xmimetype module
    - listing files in a directory
    - checking for files in the directory
    - creating a directory if it does not exist

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const fs = require('fs');
 const express = require('express');
-const xm = require("xmimetype");
+
+
+
+//const xm = require("xmimetype");
 /* 
 // naive implementation
 
@@ -13,57 +16,50 @@ app.get('/',(req,res) => {
 */
 
 // this seems to make the difference.
-// mention this in the video
 app.use(express.static('public'));
 
 app.get('/',(req,res) => {
 	// added for mime
-  res.set('Content-Type', 'text/html');
+//  res.set('Content-Type', 'text/html');
   res.sendFile(__dirname + '/public/index.html');
 });
-
-/*
-app.get('/',(req,res) => {
-	// added for mime
-  res.set('Content-Type', 'text/javascript');
-   res.sendFile(__dirname + '/public/js/socket-client.js');
-
-});*/
-/*
-app.get('/',(req,res) => {
-	// added for mime
-  res.set('Content-Type', 'text/html');
-  res.sendFile(__dirname + '/index.html');
-});*/
-
-//const path = '/js/socket.client.js';
-//app.use('/public', express.static(path.join(__dirname, "public")));
-
 io.on('connection',(socket) =>{
-	console.log('a user connected...');
-
+	console.log('a user connected... ');
+      let path = 'public/output'
+      if (!fs.existsSync(path)) {
+         fs.mkdir(path, function(err) {
+            if (err) {
+             console.log('ERROR: ' + err)
+          } else {
+          console.log("New directory successfully created.")
+          }
+       });
+     }
+     else {
+     	console.log('file exists.....');
+    }
 	socket.on('chat message', (msg) => {
 		console.log('message: ' + msg);
 
 		// file write logic
 		// alternative: fs.appendFile
-		fs.appendFile('public/messages.txt',msg + '\n',function(err){
+		fs.appendFile('public/output/messages.txt',msg + '\n',function(err){
 			if (err) throw err;
 
 			console.log('saved!');
 		});
+
+         // list files
+        fs.readdir(__dirname + '/public/output', (err, files) => {
+         files.forEach(file => {
+          console.log('filename: ' + file);
+          });
+         });
 	})
 })
-
 http.listen(3000, () => {
   console.log('listening on *:3000');
 });
 
-/*
-tutorial
 
-https://socket.io/get-started/chat
-
-Why it was useful 
-What the hiccups are
-*/
+// tutorial: https://socket.io/get-started/chat
